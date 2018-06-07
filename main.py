fe1e607b1933e4520b7215d530a36a075fb67ad1
FrancoPalau <palaufranco12@gmail.com>
2018-06-07 16:01:46 -0300

    uso de distancia euclideana

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
 import seaborn as sns
 
 
@@ -37,20 +38,34 @@ def manhattan(Wijk, x):
     return distancias
 
 
+def euclidiana(Wijk, x):
+    distancias = []
+    for i in range(NEURONAS_X):
+        distancias.append([])
+        for j in range(NEURONAS_Y):
+            dist = 0
+            for k in range(NEURONAS_ENTRADA):
+                dist += (Wijk[i][j][k] - x[k])**2
+            distancias[i].append(dist)
+    return distancias
+
+
 def main():
 
     np.random.seed(0)
     # Creacion de dataset
     data = make_blobs(n_samples=200, n_features=2,
-                      centers=4, cluster_std=1.3, random_state=102)
+                      centers=4, cluster_std=1.3, random_state=101)
     dataset = pd.DataFrame(data[0],columns="X Y".split())
     dataset["C"] = data[1]
 
     # Ploteamos el dataset
-    sns.lmplot("X","Y",dataset,hue="C",fit_reg=False)
-    plt.show()
-    #dataset = pd.read_csv("example")
-    #dataset.drop(dataset.columns[:500], axis=1, inplace=True)
+    #sns.lmplot("X","Y",dataset,hue="C",fit_reg=False)
+    #plt.show()
+    dataset = pd.read_csv("example")
+
+    # PCA
+
 
     # Inicializacion de pesos
     Wijk = np.random.randint(low=-100, high=100, size=(NEURONAS_X, NEURONAS_Y, NEURONAS_ENTRADA)) / 10000
@@ -63,20 +78,20 @@ def main():
     for it, ejemplo in enumerate(X_train.index): # Iteramos a traves de cada ejemplo de entrenamiento
         print(it)
         x = X_train.loc[ejemplo]# Vector de entrada
-        distancias = np.array(manhattan(Wijk, x))# Calculo de las distancias de todas las neuronas
+        distancias = np.array(euclidiana(Wijk, x))# Calculo de las distancias de todas las neuronas
         winner = np.unravel_index(np.argmin(distancias, axis=None), distancias.shape) # Indice de la neurona ganadora
         #Actualizamos el peso de las neuronas
         for i in range(NEURONAS_X):
             for j in range(NEURONAS_Y):
                 for k in range(NEURONAS_ENTRADA):
-                    Wijk[i][j][k] += alfa(it,tf)*h(modulo((i,j),winner),it,tf)*np.sign(x[k] - Wijk[i][j][k])
+                    Wijk[i][j][k] += alfa(it,tf)*h(modulo((i,j),winner),it,tf)*(x[k] - Wijk[i][j][k])
 
     #Un ejemplo de cada clase
     etiquetas = pd.DataFrame([dataset[dataset["C"] == 0].iloc[0],dataset[dataset["C"] == 1].iloc[0],
                               dataset[dataset["C"] == 2].iloc[0],dataset[dataset["C"] == 3].iloc[0]],
                              columns=dataset.columns)
     #etiquetas = pd.DataFrame([dataset.loc[79], dataset.loc[133], dataset.loc[109],
-    #                          dataset.loc[31], dataset.loc[0], dataset.loc[55]], columns=dataset.columns)
+    #                        dataset.loc[31], dataset.loc[0], dataset.loc[55]], columns=dataset.columns)
 
     mapa_neuronas = np.ones((NEURONAS_X, NEURONAS_Y))*-1 # Inicializamos todas las neuronas con clase -1(valor imposible)
 
@@ -85,7 +100,7 @@ def main():
     tf = len(etiquetas.index)
     for it, ejemplo in enumerate(etiquetas.index):
         x = etiquetas.loc[ejemplo]# Vector de entrada
-        distancias = np.array(manhattan(Wijk, x))# Calculo de las distancias de todas las neuronas
+        distancias = np.array(euclidiana(Wijk, x))# Calculo de las distancias de todas las neuronas
         winner = np.unravel_index(np.argmin(distancias, axis=None), distancias.shape)# Indice de la neurona ganadora
         for i in range(NEURONAS_X):
             for j in range(NEURONAS_Y):
@@ -103,13 +118,13 @@ def main():
             for j in range(NEURONAS_Y):
                 for k in range(NEURONAS_ENTRADA):
                     if y_train[ejemplo] == mapa_neuronas[i][j]:# Si concide las clases premiamos
-                        Wijk[i][j][k] += alfa_const * abs(x[k] - Wijk[i][j][k])
+                        Wijk[i][j][k] += alfa_const * (x[k] - Wijk[i][j][k])
                     else:                                      # Si no concide las clases castigamos
-                        Wijk[i][j][k] -= alfa_const * abs(x[k] - Wijk[i][j][k])
-
+                        Wijk[i][j][k] -= alfa_const * (x[k] - Wijk[i][j][k])
+    print(mapa_neuronas)
     # ----TESTEO---- #
-    x = X_test.loc[X_test.index[33]]
-    distancias = np.array(manhattan(Wijk, x))
+    x = X_test.loc[X_test.index[5]]
+    distancias = np.array(euclidiana(Wijk, x))
     # Graficamos dos Heatmaps, el primero para las neuronas que se activan
     # y el segundo con las distancias al ejemplo de testeo
     cmap = sns.cm.rocket_r
@@ -122,8 +137,8 @@ def main():
 if __name__ == "__main__":
 
     NEURONAS_ENTRADA = 2
-    NEURONAS_X = 10
-    NEURONAS_Y = 10
+    NEURONAS_X = 20
+    NEURONAS_Y = 20
     RITMO_INICIAL = 0.3
     RITMO_FINAL = 0.01
     VECINOS_INICIAL = 1
