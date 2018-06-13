310691b7563772e5bf4d4022ba984d0345bce1c7
FrancoPalau <palaufranco12@gmail.com>
2018-06-13 12:51:44 -0300

    fix some bugs

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import seaborn as sns
 
 
 def alfa(t,tf):
-    return RITMO_INICIAL+(RITMO_INICIAL - RITMO_INICIAL)*(t/tf)
+    return RITMO_INICIAL+(RITMO_FINAL - RITMO_INICIAL)*(t/tf)
 
 
 def r(t,tf):
@@ -62,31 +62,35 @@ def main():
     np.random.seed(0)
     # Creacion de dataset
     data = make_blobs(n_samples=200, n_features=2,
-                      centers=4, cluster_std=1.3, random_state=101)
+                      centers=4, cluster_std=1.1, random_state=10)
     dataset = pd.DataFrame(data[0],columns="X Y".split())
     dataset["C"] = data[1]
 
     # Ploteamos el dataset
-    #sns.lmplot("X","Y",dataset,hue="C",fit_reg=False)
-    #plt.show()
-    # dataset = pd.read_csv("example")
-    # dataset.drop(labels=dataset.columns[:500], axis=1, inplace=True)
-    # dataset2 = pd.read_csv("example")
-    dataset = pd.read_csv("College_Data")
-
-    
-    # # PCA
-    # pca = PCA(n_components=2)
-    # pca.fit(dataset.drop(labels=["C"], axis=1))
-    # dataset = pca.transform(dataset.drop(labels=["C"], axis=1))
-    # dataset= pd.DataFrame(dataset, columns=["X", "Y"])
-    # dataset["C"] = dataset2["C"]
-
+    sns.lmplot("X","Y",dataset,hue="C",fit_reg=False)
+    plt.show()
+    # -------Giroscopo ------
+    dataset = pd.read_csv("example")
+    dataset2 = pd.read_csv("example")
+    # --------College------
+    # dataset = pd.read_csv("College_Data")
+    # dataset['C'] = dataset['Private'].apply(converter)
+    # dataset.drop(["Private",dataset.columns[0]],axis=1,inplace=True)
+    # dataset2 = pd.read_csv("College_Data")
+    # dataset2['C'] = dataset2['Private'].apply(converter)
     # print(dataset.head())
     # print(dataset.info())
 
+    # PCA
+    pca = PCA(n_components=10)
+    pca.fit(dataset.drop(labels=["C"], axis=1))
+    dataset = pca.transform(dataset.drop(labels=["C"], axis=1))
+    dataset= pd.DataFrame(dataset, columns=["1", "2", "3", "4", "5", "6", "7","8", "9","10"])
+    dataset["C"] = dataset2["C"]
+
     # Inicializacion de pesos
-    Wijk = np.random.randint(low=-100, high=100, size=(NEURONAS_X, NEURONAS_Y, NEURONAS_ENTRADA)) / 10000
+    Wijk = np.random.randint(low=-100, high=100, size=(NEURONAS_X, NEURONAS_Y, NEURONAS_ENTRADA))/10000
+    # Wijk = np.random.randn(NEURONAS_X, NEURONAS_Y, NEURONAS_ENTRADA)
 
     # Dividimos el dataset en entrenamiento y testeo(X=entradas, y=salidas)
     X_train, X_test, y_train, y_test = train_test_split(dataset[dataset.columns[:-1]],dataset["C"],
@@ -96,22 +100,20 @@ def main():
     tf = len(X_train.index) # Cantidad de ejemplos(iteraciones)
     for it, ejemplo in enumerate(X_train.index): # Iteramos a traves de cada ejemplo de entrenamiento
         print(it)
-        x = X_train.loc[ejemplo]# Vector de entrada
-        distancias = np.array(manhattan(Wijk, x))# Calculo de las distancias de todas las neuronas
-        winner = np.unravel_index(np.argmin(distancias, axis=None), distancias.shape) # Indice de la neurona ganadora
+        x = X_train.loc[ejemplo]  # Vector de entrada
+        distancias = np.array(manhattan(Wijk, x))  # Calculo de las distancias de todas las neuronas
+        winner = np.unravel_index(np.argmin(distancias, axis=None), distancias.shape)  # Indice de la neurona ganadora
         #Actualizamos el peso de las neuronas
         for i in range(NEURONAS_X):
             for j in range(NEURONAS_Y):
                 for k in range(NEURONAS_ENTRADA):
-                    Wijk[i][j][k] += alfa(it,tf)*h(modulo((i,j),winner),it,tf)*np.sign(x[k] - Wijk[i][j][k])
+                        Wijk[i][j][k] += alfa(it,tf)*h(modulo((i,j),winner),it,tf)*np.sign(x[k] - Wijk[i][j][k])
 
     #Un ejemplo de cada clase
     etiquetas = pd.DataFrame([dataset[dataset["C"] == 1].iloc[0],dataset[dataset["C"] == 2].iloc[0],
                               dataset[dataset["C"] == 3].iloc[0],dataset[dataset["C"] == 4].iloc[0],
                               dataset[dataset["C"] == 5].iloc[0], dataset[dataset["C"] == 6].iloc[0]],
                              columns=dataset.columns)
-    #etiquetas = pd.DataFrame([dataset.loc[79], dataset.loc[133], dataset.loc[109],
-    #                        dataset.loc[31], dataset.loc[0], dataset.loc[55]], columns=dataset.columns)
 
     mapa_neuronas = np.ones((NEURONAS_X, NEURONAS_Y))*-1 # Inicializamos todas las neuronas con clase -1(valor imposible)
 
@@ -119,51 +121,36 @@ def main():
     # neurona ganadora y a sus vecinos
     tf = len(etiquetas.index)
     for it, ejemplo in enumerate(etiquetas.index):
-        x = etiquetas.loc[ejemplo]# Vector de entrada
-        distancias = np.array(manhattan(Wijk, x))# Calculo de las distancias de todas las neuronas
-        winner = np.unravel_index(np.argmin(distancias, axis=None), distancias.shape)# Indice de la neurona ganadora
+        x = etiquetas.loc[ejemplo]  # Vector de entrada
+        distancias = np.array(manhattan(Wijk, x))  # Calculo de las distancias de todas las neuronas
+        winner = np.unravel_index(np.argmin(distancias, axis=None), distancias.shape)  # Indice de la neurona ganadora
         for i in range(NEURONAS_X):
             for j in range(NEURONAS_Y):
-                ind_otra = (i, j)
-                if h(modulo(ind_otra,winner),0,tf):# Si la neurona es vecina de la ganadora, le asignamos la clase
-                    mapa_neuronas[i][j] = etiquetas.loc[ejemplo]["C"]
-        mapa_neuronas[winner[0]][winner[1]] = etiquetas.loc[ejemplo]["C"]# Asignamos la clase mostrada a la ganadora
-
-    # ----TESTEO---- #
-    x = X_test.loc[X_test.index[25]]
-    distancias = np.array(manhattan(Wijk, x))
-    # Graficamos dos Heatmaps, el primero para las neuronas que se activan
-    # y el segundo con las distancias al ejemplo de testeo
-    cmap = sns.cm.rocket_r
-    sns.heatmap(mapa_neuronas, cmap=cmap, linecolor='white', linewidths=1)
-    plt.show()
-    sns.heatmap(distancias, cmap="magma", linecolor='white', linewidths=1)
-    plt.show()
-
+                if h(modulo((i, j),winner),0,tf):              # Si la neurona es vecina de la ganadora (o la ganadora),
+                    mapa_neuronas[i][j] = etiquetas.loc[ejemplo]["C"]  # le asignamos la clase
 
     # --------- Ajuste fino LVQ --------- #
     print("LVQ in progress....")
-    alfa_const = 0.01 # Ahora el ritmo de aprendizaje es constante
+    alfa_const = 0.01  # Ahora el ritmo de aprendizaje es constante
     for it, ejemplo in enumerate(X_train.index):
-        x = X_train.loc[ejemplo]# Vector de entrada
+        x = X_train.loc[ejemplo]  # Vector de entrada
         for i in range(NEURONAS_X):
             for j in range(NEURONAS_Y):
                 for k in range(NEURONAS_ENTRADA):
-                    if y_train[ejemplo] == mapa_neuronas[i][j]:# Si concide las clases premiamos
+                    if y_train[ejemplo] == mapa_neuronas[i][j]:  # Si concide las clases premiamos
                         Wijk[i][j][k] += alfa_const * np.sign(x[k] - Wijk[i][j][k])
-                    else:                                      # Si no concide las clases castigamos
+                    else:                                        # Si no concide las clases castigamos
                         Wijk[i][j][k] -= alfa_const * np.sign(x[k] - Wijk[i][j][k])
-    print(mapa_neuronas)
+
     # ----TESTEO---- #
-    x = X_test.loc[X_test.index[25]]
-    distancias = np.array(manhattan(Wijk, x))
-    # Graficamos dos Heatmaps, el primero para las neuronas que se activan
-    # y el segundo con las distancias al ejemplo de testeo
     cmap = sns.cm.rocket_r
-    sns.heatmap(mapa_neuronas, cmap=cmap,linecolor='white',linewidths=1)
+    sns.heatmap(mapa_neuronas, cmap=cmap, linecolor='white', linewidths=1, annot=True)  #Heatmap con las neuronas que se activan
     plt.show()
-    sns.heatmap(distancias, cmap="magma", linecolor='white', linewidths=1)
-    plt.show()
+    for ejemplo in X_test.index:  #Mostramos de a un ejemplo del conjunto de testeo y graficamos las distancias
+        x = X_test.loc[ejemplo]
+        distancias = np.array(manhattan(Wijk, x))
+        sns.heatmap(distancias, cmap="magma", linecolor='white', linewidths=1, annot=True)
+        plt.show()
 
 
 if __name__ == "__main__":
